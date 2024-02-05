ad2c462e2c967a7a2a584904964099529b6e0e37
NTDuyy30 <nguyentienduy14119@gmail.com>
2024-02-05 23:07:48 +0700

    Pagination

---
+++ src/main/java/com/ttcn/controller/admin/api/CategoryAPI.java
@@ -0,0 +1,50 @@
+package com.ttcn.controller.admin.api;
+
+import java.io.IOException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ttcn.model.CategoryModel;
+import com.ttcn.service.ICategoryService;
+import com.ttcn.utils.HttpUtil;
+
+@WebServlet(urlPatterns = {"/api-admin-category"})
+public class CategoryAPI extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	@Inject
+	private ICategoryService categoryService;
+       
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		ObjectMapper mapper = new ObjectMapper();
+		CategoryModel categoryModel =  HttpUtil.of(request.getReader()).toModel(CategoryModel.class);
+		categoryModel = this.categoryService.insert(categoryModel);
+		mapper.writeValue(response.getOutputStream(), categoryModel);
+	}
+
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		ObjectMapper mapper = new ObjectMapper();
+		CategoryModel categoryModel =  HttpUtil.of(request.getReader()).toModel(CategoryModel.class);
+		categoryModel = this.categoryService.update(categoryModel);
+		mapper.writeValue(response.getOutputStream(), categoryModel);
+	}
+
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("application/json");
+		ObjectMapper mapper = new ObjectMapper();
+		CategoryModel categoryModel =  HttpUtil.of(request.getReader()).toModel(CategoryModel.class);
+		this.categoryService.delete(categoryModel.getIds());
+		mapper.writeValue(response.getOutputStream(), "{}");
+	}
+
+}
